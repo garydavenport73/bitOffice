96e8b1e4913d75c5bb5862e0ef5e0f56b46897e2
garydavenport73 <software@garydavenport.com>
2022-07-08 19:45:29 -0400

    add license, rearranging folder contents

--- makeOneSmallFile.sh
+++ makeOneSmallFile.sh
@@ -1,8 +1,6 @@
-csplit index.html /splitsplitsplit/ {*}
+csplit bitoffice.html /splitsplitsplit/ {*}
 
 cat globalVariables.js commonFunctions.js notes_javascript.js write_javascript.js tables_javascript.js contacts_javascript.js calendar_javascript.js calculator_javascript.js startup.js >javascript.js
-
-
 
 #sed -i 's/<!--splitsplitsplit-->//g' javascript.js
 #sed -i 's/\/\/splitsplitsplit//g' javascript.js
@@ -18,10 +16,13 @@ sed -i 's/\/\/splitsplitsplit//g' xx05
 sed -i 's/<!--splitsplitsplit-->//g' xx06
 sed -i 's/\/\/splitsplitsplit//g' xx06
 
-
 minify -o javascript.min.js javascript.js
 minify -o style.min.css style.css
 
-cat xx00 xx02 style.min.css xx03 xx05 javascript.min.js xx06 >test.html
+cat xx00 xx02 style.min.css xx03 xx05 javascript.min.js xx06 >deleteme.html
 
-rm -rf xx00 xx01 xx02 xx03 xx04 xx05 xx06 style.min.css javascript.min.js javascript.js
+minify -o index.html deleteme.html
+
+rm -rf xx00 xx01 xx02 xx03 xx04 xx05 xx06 style.min.css javascript.min.js javascript.js deleteme.html
+
+
